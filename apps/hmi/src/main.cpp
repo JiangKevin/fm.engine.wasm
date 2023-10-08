5b7e41266a28f4f7ff718d21f086a941b4ed79ba
JiangKevin <JiangKevin@users.noreply.github.com>
2023-10-08 10:41:52 +0800

    备份一下

--- apps/hmi/src/main.cpp
+++ apps/hmi/src/main.cpp
@@ -2,30 +2,27 @@
 //  Author: Shervin Aflatooni
 //
 
-#include "Engine.h"
-
-#include "components/MeshRenderer.h"
-#include "components/PerspectiveCamera.h"
-#include "components/OrthoCamera.h"
-#include "components/FreeMove.h"
-#include "components/FreeLook.h"
-#include "components/DirectionalLight.h"
-#include "components/SpotLight.h"
-#include "components/PointLight.h"
-#include "components/BoxCollider.h"
-#include "components/SphereCollider.h"
-
-#include "Plane.h"
-#include "Mesh.h"
-#include "Texture.h"
-#include "Logger.h"
-#include "MeshLoader.h"
-
 #include <SDL_main.h>
 
-class CoolGame : public Game
-{
-public:
+#include "Engine.h"
+#include "Logger.h"
+#include "Mesh.h"
+#include "MeshLoader.h"
+#include "Plane.h"
+#include "Texture.h"
+#include "components/BoxCollider.h"
+#include "components/DirectionalLight.h"
+#include "components/FreeLook.h"
+#include "components/FreeMove.h"
+#include "components/MeshRenderer.h"
+#include "components/OrthoCamera.h"
+#include "components/PerspectiveCamera.h"
+#include "components/PointLight.h"
+#include "components/SphereCollider.h"
+#include "components/SpotLight.h"
+
+class CoolGame : public Game {
+ public:
   virtual void init(GLManager *glManager);
   virtual void update(Input *input, std::chrono::microseconds delta);
 
@@ -33,24 +30,24 @@ public:
   std::shared_ptr<PerspectiveCamera> primary_camera2;
 };
 
-void CoolGame::update(Input *input, std::chrono::microseconds delta)
-{
+void CoolGame::update(Input *input, std::chrono::microseconds delta) {
   Game::update(input, delta);
 }
 
-void CoolGame::init(GLManager *glManager)
-{
+void CoolGame::init(GLManager *glManager) {
   auto input = getEngine()->getWindow()->getInput();
   input->registerKeyToAction(SDLK_SPACE, "fire");
   input->registerKeyToAction(SDLK_c, "swapCamera");
 
   input->bindAction("fire", IE_PRESSED, [this]() {
     MeshLoader cube("cube.obj");
-    cube.getEntity()->getTransform().setPosition(primary_camera->getParent()->getPosition());
+    cube.getEntity()->getTransform().setPosition(
+        primary_camera->getParent()->getPosition());
     cube.getEntity()->addComponent<BoxCollider>(glm::vec3(0.5, 0.5, 0.5), 50);
     addToScene(cube.getEntity());
     auto dir = primary_camera->getParent()->getDirection();
-    cube.getEntity()->getComponent<BoxCollider>()->applyCentralImpulse(glm::vec3(dir.x * 500.0f, dir.y * 500.0f, dir.z * 500.0f));
+    cube.getEntity()->getComponent<BoxCollider>()->applyCentralImpulse(
+        glm::vec3(dir.x * 500.0f, dir.y * 500.0f, dir.z * 500.0f));
   });
 
   input->bindAction("swapCamera", IE_PRESSED, [this]() {
@@ -61,13 +58,18 @@ void CoolGame::init(GLManager *glManager)
     getEngine()->getGLManager()->setActiveCamera(primary_camera);
   });
 
-  auto brickMat = std::make_shared<Material>(std::make_shared<Texture>(Asset("bricks2.jpg")), std::make_shared<Texture>(Asset("bricks2_normal.jpg")), std::make_shared<Texture>(Asset("bricks2_specular.png")));
+  auto brickMat = std::make_shared<Material>(
+      std::make_shared<Texture>(Asset("bricks2.jpg")),
+      std::make_shared<Texture>(Asset("bricks2_normal.jpg")),
+      std::make_shared<Texture>(Asset("bricks2_specular.png")));
   auto planeMesh = Plane::getMesh();
   // ground
   {
     auto plane = std::make_shared<Entity>();
     plane->addComponent<MeshRenderer>(planeMesh, brickMat);
-    plane->getTransform().setPosition(glm::vec3(-5, -2, 0)).setScale(glm::vec3(10, 1, 10));
+    plane->getTransform()
+        .setPosition(glm::vec3(-5, -2, 0))
+        .setScale(glm::vec3(10, 1, 10));
     plane->addComponent<BoxCollider>(glm::vec3(5, 0, 5), 0);
 
     addToScene(plane);
@@ -75,7 +77,9 @@ void CoolGame::init(GLManager *glManager)
   {
     auto plane = std::make_shared<Entity>();
     plane->addComponent<MeshRenderer>(planeMesh, brickMat);
-    plane->getTransform().setPosition(glm::vec3(5, -2, 0)).setScale(glm::vec3(10, 1, 10));
+    plane->getTransform()
+        .setPosition(glm::vec3(5, -2, 0))
+        .setScale(glm::vec3(10, 1, 10));
     plane->addComponent<BoxCollider>(glm::vec3(5, 0, 5), 0);
 
     addToScene(plane);
@@ -83,7 +87,9 @@ void CoolGame::init(GLManager *glManager)
   {
     auto plane = std::make_shared<Entity>();
     plane->addComponent<MeshRenderer>(planeMesh, brickMat);
-    plane->getTransform().setPosition(glm::vec3(-5, -2, 10)).setScale(glm::vec3(10, 1, 10));
+    plane->getTransform()
+        .setPosition(glm::vec3(-5, -2, 10))
+        .setScale(glm::vec3(10, 1, 10));
     plane->addComponent<BoxCollider>(glm::vec3(5, 0, 5), 0);
 
     addToScene(plane);
@@ -91,7 +97,9 @@ void CoolGame::init(GLManager *glManager)
   {
     auto plane = std::make_shared<Entity>();
     plane->addComponent<MeshRenderer>(planeMesh, brickMat);
-    plane->getTransform().setPosition(glm::vec3(5, -2, 10)).setScale(glm::vec3(10, 1, 10));
+    plane->getTransform()
+        .setPosition(glm::vec3(5, -2, 10))
+        .setScale(glm::vec3(10, 1, 10));
     plane->addComponent<BoxCollider>(glm::vec3(5, 0, 5), 0);
 
     addToScene(plane);
@@ -195,13 +203,11 @@ void CoolGame::init(GLManager *glManager)
 
   /*{
     MeshLoader ml("Pregnant.obj");
-    ml.getEntity()->getTransform().setPosition(glm::vec3(0 + (i * 3), -2, -2.5));
-    ml.getEntity()->addComponent<Sphere>(1);
-    addToScene(ml.getEntity());
+    ml.getEntity()->getTransform().setPosition(glm::vec3(0 + (i * 3), -2,
+  -2.5)); ml.getEntity()->addComponent<Sphere>(1); addToScene(ml.getEntity());
   }*/
 
-  for (int i = 0; i < 10; i++)
-  {
+  for (int i = 0; i < 10; i++) {
     MeshLoader ml("AncientUgandan.obj");
     ml.getEntity()->getTransform().setPosition(glm::vec3(0, i * 3, -2.5));
     ml.getEntity()->addComponent<SphereCollider>(1, 1);
@@ -210,21 +216,35 @@ void CoolGame::init(GLManager *glManager)
 
   MeshLoader money("monkey3.obj");
   money.getEntity()->getTransform().setPosition(glm::vec3(0, 0, 8));
-  money.getEntity()->addComponent<PerspectiveCamera>(glm::pi<float>() / 2.0f, getEngine()->getWindow()->getWidth() / (float)getEngine()->getWindow()->getHeight(), 0.05f, 100.0f);
-  //money.getEntity()->addComponent<SpotLight>(glm::vec3(0.1f, 1.0f, 1.0f), 5.8f, 0.7f, std::make_shared<Attenuation>(0, 0, 0.2));
+  money.getEntity()->addComponent<PerspectiveCamera>(
+      glm::pi<float>() / 2.0f,
+      getEngine()->getWindow()->getWidth() /
+          (float)getEngine()->getWindow()->getHeight(),
+      0.05f, 100.0f);
+  // money.getEntity()->addComponent<SpotLight>(glm::vec3(0.1f, 1.0f, 1.0f), 5.8f,
+  // 0.7f, std::make_shared<Attenuation>(0, 0, 0.2));
   money.getEntity()->addComponent<SphereCollider>(1, 1);
   addToScene(money.getEntity());
 
   MeshLoader money2("monkey3.obj");
-  money2.getEntity()->addComponent<PerspectiveCamera>(glm::pi<float>() / 2.0f, getEngine()->getWindow()->getWidth() / (float)getEngine()->getWindow()->getHeight(), 0.8f, 100.0f);
+  money2.getEntity()->addComponent<PerspectiveCamera>(
+      glm::pi<float>() / 2.0f,
+      getEngine()->getWindow()->getWidth() /
+          (float)getEngine()->getWindow()->getHeight(),
+      0.8f, 100.0f);
   money2.getEntity()->addComponent<FreeMove>();
 #if defined(ANDROID)
   money2.getEntity()->addComponent<FreeLook>(0.1f);
 #else
   money2.getEntity()->addComponent<FreeLook>();
 #endif
-  money2.getEntity()->getTransform().setPosition(glm::vec3(0, 0, 5)).setScale(glm::vec3(0.8, 0.8, 0.8));
-  money2.getEntity()->addComponent<SpotLight>(glm::vec3(1.0f, 1.0f, 1.0f), 2.8f, 0.7f, std::make_shared<Attenuation>(0, 0, 0.2));
+  money2.getEntity()
+      ->getTransform()
+      .setPosition(glm::vec3(0, 0, 5))
+      .setScale(glm::vec3(0.8, 0.8, 0.8));
+  money2.getEntity()->addComponent<SpotLight>(
+      glm::vec3(1.0f, 1.0f, 1.0f), 2.8f, 0.7f,
+      std::make_shared<Attenuation>(0, 0, 0.2));
 
   addToScene(money2.getEntity());
 
@@ -234,8 +254,7 @@ void CoolGame::init(GLManager *glManager)
   getEngine()->getGLManager()->setActiveCamera(primary_camera);
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   CoolGame game;
   Engine gameEngine(&game);
 

5a1f408f79817d2fbea6a9ed307f031b1a59f754
JiangKevin <JiangKevin@users.noreply.github.com>
2023-10-08 10:21:00 +0800

    备份一下

--- engine.core/scripts/cmake-emscripten.sh
+++ engine.core/scripts/cmake-emscripten.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-
+#
+reset
+clear
+#
 set -e
 
 BUILD_TYPE=${BUILD_TYPE:-Release}
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir -p $DIR/../bin-emscripten
-mkdir -p $DIR/../bin-emscripten/bin/
-cp $DIR/../src/example/html/index.html $DIR/../bin-emscripten/bin/
-cd $DIR/../bin-emscripten
-cmake -DEMSCRIPTEN=1 -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_TOOLCHAIN_FILE=$EMSCRIPTEN/cmake/Modules/Platform/Emscripten.cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE ../
-make $*
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+mkdir -p $DIR/bin-emscripten
+mkdir -p $DIR/bin-emscripten/bin/
+cp $DIR/engine.core/src/example/html/index.html $DIR/bin-emscripten/bin/
+#
+
+emcmake cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -S $DIR/engine.core -B $DIR/bin-emscripten
+# 
+cd $DIR/bin-emscripten
+make
